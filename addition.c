9eaac5cdcf69203b142945ec02a76bc945ecb723
gourav512 <32830874+gourav512@users.noreply.github.com>
2020-09-12 23:41:03 +0530

    commit

--- addition.c
+++ addition.c
@@ -1,52 +1,46 @@
-#include<stdio.h>
-#include<stdlib.h>
+// gourav ji
+
+#include <stdio.h>
+#include <stdlib.h>
 void main()
 {
-    int a[2][2],i,j,b[2][2],c[2][2],r1,r2,c1,c2,s,m;
+    int a[2][2], i, j, b[2][2], c[2][2], r1, r2, c1, c2, s, m;
     printf("Enter order of matrix A : ");
-    scanf("%d%d",&r1,&c1);
+    scanf("%d%d", &r1, &c1);
     printf("Enter order of matrix B : ");
-    scanf("%d%d",&r2,&c2);
-    if(c1 != r2)
+    scanf("%d%d", &r2, &c2);
+    if (c1 != r2)
     {
         printf("Order of matrix is not same addition not possible ");
         exit(1);
     }
     printf("\nEnter element in matrix A : ");
-    for(i=0;i<r1;i++)
-    for(j=0;j<c1;j++)
-    {
-        printf("\nEnter element A %d,%d location : ",i,j);
-        scanf("%d",&a[i][j]);
-    }
+    for (i = 0; i < r1; i++)
+        for (j = 0; j < c1; j++)
+        {
+            printf("\nEnter element A %d,%d location : ", i, j);
+            scanf("%d", &a[i][j]);
+        }
     printf("\nEnter element in matrix B : ");
-    for(i=0;i<r2;i++)
-    for(j=0;j<c2;j++)
-    {
-        printf("\nEnter element B %d,%d location : ",i,j);
-        scanf("%d",&b[i][j]);
-    }
-    for(i=0;i<r1;i++)
-    for(j=0;j<c2;j++)
-    {
-        s=0;
-        for(m=0;m<c1;m++)
-        s=s+a[i][m]*b[m][j];
-        c[i][j]=s;
-    }
+    for (i = 0; i < r2; i++)
+        for (j = 0; j < c2; j++)
+        {
+            printf("\nEnter element B %d,%d location : ", i, j);
+            scanf("%d", &b[i][j]);
+        }
+    for (i = 0; i < r1; i++)
+        for (j = 0; j < c2; j++)
+        {
+            s = 0;
+            for (m = 0; m < c1; m++)
+                s = s + a[i][m] * b[m][j];
+            c[i][j] = s;
+        }
     printf("\nResult\n");
-    for(i=0;i<r1;i++)
+    for (i = 0; i < r1; i++)
     {
-    for(j=0;j<c2;j++)
-        printf("\t%d ",c[i][j]);
-        printf("\n"); 
+        for (j = 0; j < c2; j++)
+            printf("\t%d ", c[i][j]);
+        printf("\n");
     }
 }
-
-
-
-
-
-
-
-
